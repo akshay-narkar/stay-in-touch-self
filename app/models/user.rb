60baff868ca30678b7783d479238c643d9b4d013
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-03 17:32:12 +0530

    Add user association with friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, foreign_key: :friend_id, class_name: :Friendship
 end
