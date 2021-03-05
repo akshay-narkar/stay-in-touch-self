0e15486b4289e2079b6473afeae8f367fb2bf412
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-05 19:22:53 +0530

    Add scopes and unique validation to friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,15 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
+  validates_uniqueness_of :user, scope: %i[friend]
+  # scope :past, -> { where('eventdate <=  ?', current_user.id) }
+  # scope :friendlist, lambda {
+  #                      where('user_id > ?', current_user.id).or(where('friend_id > ?', current_user.id))
+  #                        .and(where('status == ?', 1))
+  #                    }
+  scope :friendlist, -> {where('status = ?', true)}
+  scope :waiting, -> {where('status = ?', false)}
+  # OR ('friend_id > ?',current_user.id)) AND ('status == ?',1 )) }
+  # scope :past, -> { where('eventdate <=  ?', Date.today) }
+  # Post.where(id: 1).or(Post.where(id: 2))
 end
