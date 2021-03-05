5ad023bc146f4d153ab3c48f459effd3494281c4
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-05 19:25:12 +0530

    Add routes for friendships

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:index, :show] do
-    resources :friendships, only: [:create,:destory,:index,:show] 
-  end
+  resources :users, only: [:index, :show] 
+    resources :friendships
+    # , only: [:create,:destory,:index,:show] 
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
