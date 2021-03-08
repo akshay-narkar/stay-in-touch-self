1e175ae21e0668c5f5413507c183c668faf387e8
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-09 03:11:39 +0530

    Change the friends method and write the logic in a better way

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -22,21 +22,26 @@ class Friendship < ApplicationRecord
   # end
 
   def await(current_user, user)
-    Friendship.where('friend_id = ?', current_user.id).or(Friendship.where('user_id = ?',
-                                                                           user.id)).or(Friendship.where('friend_id = ?',
-                                                                                                         user.id)).or(Friendship.where(
-                                                                                                                        'user_id = ?', current_user.id
-                                                                                                                      )).waiting
+    x = Friendship.where('friend_id = ?', current_user.id).and(Friendship.where('user_id = ?', user.id)).waiting
+    x1 = Friendship.where('friend_id = ?', user.id).and(Friendship.where('user_id = ?', current_user.id)).waiting
+
+    x2 = x + x1
+
+    x2.empty? ? nil : x2
   end
 
   def friendss(current_user, user)
-    Friendship.where('friend_id = ?', current_user.id).or(Friendship.where('user_id = ?',
-                                                                           user.id)).or(Friendship.where('friend_id = ?',
-                                                                                                         user.id))
-      .or(Friendship.where('user_id = ?', current_user.id)).friendlist
+    y = Friendship.where('friend_id = ?', current_user.id).and(Friendship.where('user_id = ?',
+                                                                                user.id)).friendlist
+
+    y1 = Friendship.where('friend_id = ?', user.id).and(Friendship.where('user_id = ?', current_user.id)).friendlist
+
+    y2 = y + y1
+
+    y2.empty? ? nil : y2
   end
 
   def pendingreq(currentuser)
-      Friendship.where('friend_id = ?', currentuser.id).waiting
+    Friendship.where('friend_id = ?', currentuser.id).waiting
   end
 end
