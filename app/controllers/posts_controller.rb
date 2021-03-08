0bd88c5d39b589f7bde62db78b168570c05823e3
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-08 17:02:55 +0530

    Remove comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
   def timeline_posts
     # friendobject = Friendship.new
     # postsbyfriends = friendobject.friendpost(current_user)
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user) 
+    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
   end
 
   def post_params
