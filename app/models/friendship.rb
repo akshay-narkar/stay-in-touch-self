c084f6cd5db4a1a89d30f6449ffe0afe70eb793f
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-08 14:57:47 +0530

    Add methods to friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -7,9 +7,32 @@ class Friendship < ApplicationRecord
   #                      where('user_id > ?', current_user.id).or(where('friend_id > ?', current_user.id))
   #                        .and(where('status == ?', 1))
   #                    }
-  scope :friendlist, -> {where('status = ?', true)}
-  scope :waiting, -> {where('status = ?', false)}
+  scope :friendlist, -> { where('status = ?', true) }
+  scope :waiting, -> { where('status = ?', false) }
   # OR ('friend_id > ?',current_user.id)) AND ('status == ?',1 )) }
   # scope :past, -> { where('eventdate <=  ?', Date.today) }
   # Post.where(id: 1).or(Post.where(id: 2))
+
+  # # def isfriend?
+  #         @awaitng = (Friendship.where('friend_id = ?', current_user.id)).waiting
+  # end
+
+  # def awaitingconf
+
+  # end
+
+  def await(current_user, user)
+    Friendship.where('friend_id = ?', current_user.id).or(Friendship.where('user_id = ?',
+                                                                           user.id)).or(Friendship.where('friend_id = ?',
+                                                                                                         user.id)).or(Friendship.where(
+                                                                                                                        'user_id = ?', current_user.id
+                                                                                                                      )).waiting
+  end
+
+  def friendss(current_user, user)
+    Friendship.where('friend_id = ?', current_user.id).or(Friendship.where('user_id = ?',
+                                                                           user.id)).or(Friendship.where('friend_id = ?',
+                                                                                                         user.id))
+      .or(Friendship.where('user_id = ?', current_user.id)).friendlist
+  end
 end
