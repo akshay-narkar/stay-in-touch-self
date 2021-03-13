01dc2fd3d7fee789c7b3903f6c9cf0c99fcb0ee5
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-13 22:04:28 +0530

    Create 2 rows for a friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,24 +4,10 @@ class Friendship < ApplicationRecord
   validates_uniqueness_of :user, scope: %i[friend]
   validates_uniqueness_of :friend, scope: %i[user]
 
-  # scope :past, -> { where('eventdate <=  ?', current_user.id) }
-  # scope :friendlist, lambda {
-  #                      where('user_id > ?', current_user.id).or(where('friend_id > ?', current_user.id))
-  #                        .and(where('status == ?', 1))
-  #                    }
-  scope :friendlist, -> { where('status = ?', true) }
+  # scope :friendlist, -> { where('status = ?', true) }
   scope :waiting, -> { where('status = ?', false) }
-  # OR ('friend_id > ?',current_user.id)) AND ('status == ?',1 )) }
-  # scope :past, -> { where('eventdate <=  ?', Date.today) }
-  # Post.where(id: 1).or(Post.where(id: 2))
 
-  # # def isfriend?
-  #         @awaitng = (Friendship.where('friend_id = ?', current_user.id)).waiting
-  # end
-
-  # def awaitingconf
-
-  # end
+  after_update :create_friend
 
   def await(current_user, user)
     x = Friendship.where('friend_id = ?', current_user.id).and(Friendship.where('user_id = ?', user.id)).waiting
@@ -33,12 +19,14 @@ class Friendship < ApplicationRecord
   end
 
   def friendss(current_user, user)
-    y = Friendship.where('friend_id = ?', current_user.id).and(Friendship.where('user_id = ?',
-                                                                                user.id)).friendlist
+    # y = Friendship.where('friend_id = ?', current_user.id).and(Friendship.where('user_id = ?',
+    #                                                                             user.id)).friendlist
 
-    y1 = Friendship.where('friend_id = ?', user.id).and(Friendship.where('user_id = ?', current_user.id)).friendlist
+    # y1 = Friendship.where('friend_id = ?', user.id).and(Friendship.where('user_id = ?', current_user.id)).friendlist
 
-    y2 = y + y1
+    # y2 = y + y1
+
+    y2 = current_user.friendships
 
     y2.empty? ? nil : y2
   end
@@ -46,4 +34,11 @@ class Friendship < ApplicationRecord
   def pendingreq(currentuser)
     Friendship.where('friend_id = ?', currentuser.id).waiting
   end
+
+  private 
+
+  def create_friend
+    Friendship.create(status: true, friend_id: self.user_id, user_id: self.friend_id)
+  end
+
 end
